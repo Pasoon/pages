25764e48079d31196b068efa8af6cd75d0130bbf
Pasoon Azimi <pasoon.azimi10@gmail.com>
2017-06-30 23:46:03 -0400

    added html files for other pages, cart now increases when add to cart is clicked for a book

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
+var cartcount=0;
+
 function updateBrowsingHeader() {
     document.getElementById("browsetext");
     
+}
+
+function addToCart(){
+    ++cartcount;
+    document.getElementById("cart").innerHTML = "Cart("+cartcount+")";
 }
